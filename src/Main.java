d1fc6624508c44fbb9a3e1c9ace8bbb69ce46432
Samuel <samueltportes@gmail.com>
2025-03-27 17:30:40 -0300

    Inicio da lógica de inserções diferentes

--- src/Main.java
+++ src/Main.java
@@ -7,24 +7,28 @@ public class Main {
         String number, upperCaseLetters, specialCharacters, password = "";
         int charactersAmount;
         System.out.println("Bem vindo ao gerador de senhas automático! \nApós essa mensagem serão feitas perguntas para a geração de senhas da sua preferência\nObs: Por padrão as senhas já são geradas com letras minúsculas!");
+
         System.out.println("Quantos caracteres você deseja?");
         charactersAmount = sc.nextInt();
-        System.out.println("Você quer que a sua senha seja gerada com letras Maiúsculas?\nEscreva 'Sim' ou 'Não");
+
+        System.out.println("Você quer que a sua senha seja gerada com letras Maiúsculas?\nEscreva 'S' ou 'N");
         upperCaseLetters = sc.next();
-        if(Objects.equals(upperCaseLetters, "Sim")) System.out.println("Você digitou Sim");else System.out.println("Você digitou Não");
-        System.out.println("Você quer que a sua senha seja gerada com números?\nEscreva 'Sim' ou 'Não");
+
+        System.out.println("Você quer que a sua senha seja gerada com números?\nEscreva 'S' ou 'N");
         number = sc.next();
-        System.out.println("Você quer que a sua senha seja gerada com caracteres especiais? Ex: %$#@!&*()_-=+ \nEscreva 'Sim' ou 'Não");
+
+        System.out.println("Você quer que a sua senha seja gerada com caracteres especiais? Ex: %$#@!&*()_-=+ \nEscreva 'S' ou 'N");
         specialCharacters = sc.next();
 
         for(int i = 1; i <= charactersAmount; i++){
-            password += escolherLetraAleatoria();
+            password += escolherLetraAleatoria(Objects.equals(upperCaseLetters, "S"),Objects.equals(number, "S"),Objects.equals(specialCharacters, "S"));
             System.out.println(password);
         }
     }
 
-    public static String escolherLetraAleatoria(){
-        String[] letters = {"a","b","c","d","e"};
+    public static String escolherLetraAleatoria(boolean uppercase, boolean numbers, boolean special){
+        String[] letters = "abcdefghijklmnopqrstuvwxyz".split("");
+        String[] lettersUpperCase = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
         String chosedLetter = "";
         int random = new Random().nextInt(letters.length);
         chosedLetter = letters[random];
